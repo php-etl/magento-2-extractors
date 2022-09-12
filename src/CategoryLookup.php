690e01a151e21de015ac4f60024f6c315ed1a6f7
sebprt <sebastien@kiboko.fr>
2022-09-12 15:52:48 +0200

    Created lookup for Category, Collection and Family

---
+++ src/CategoryLookup.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kiboko\Component\Flow\Magento2;
+
+use Kiboko\Component\Bucket\ComplexResultBucket;
+use Kiboko\Contract\Pipeline\TransformerInterface;
+
+final class CategoryLookup implements TransformerInterface
+{
+    public function __construct(
+        private \Psr\Log\LoggerInterface $logger,
+        private \Kiboko\Magento\V2_1\Client|\Kiboko\Magento\V2_2\Client|\Kiboko\Magento\V2_3\Client|\Kiboko\Magento\V2_4\Client $client,
+    ) {
+    }
+
+    public function transform(): \Generator
+    {
+        $line = yield;
+        do {
+            $bucket = new ComplexResultBucket();
+            $output = $line;
+
+            (function ($input, $bucket) use ($output) {
+                try {
+                    $lookup = $this->client->catalogCategoryAttributeOptionManagementV1GetItemsGet(
+                        attributeCode: $input["Product_Category"],
+                    );
+
+                    if (!$lookup instanceof \Kiboko\Magento\V2_1\Model\EavDataAttributeOptionInterface
+                        && !$lookup instanceof \Kiboko\Magento\V2_2\Model\EavDataAttributeOptionInterface
+                        && !$lookup instanceof \Kiboko\Magento\V2_3\Model\EavDataAttributeOptionInterface
+                        && !$lookup instanceof \Kiboko\Magento\V2_4\Model\EavDataAttributeOptionInterface
+                    ) {
+                        return;
+                    }
+                } catch (\RuntimeException $exception) {
+                    $this->logger->warning($exception->getMessage(), ['exception' => $exception, 'item' => $input]);
+                    $bucket->reject($input);
+                    return;
+                }
+
+                $output = (function () use ($lookup, $output) {
+                    $output['Product_Category'] = $lookup->getLabel();
+                    return $output;
+                })();
+
+                $bucket->accept($output);
+            })($line, $bucket);
+        } while ($line = (yield $bucket));
+    }
+}
