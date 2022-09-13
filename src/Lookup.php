eeab9acb45020dd548bea1430326f178bbdfa149
sebprt <sebastien@kiboko.fr>
2022-09-13 16:41:54 +0200

    Removed duplicate classes

--- src/Lookup.php
+++ src/Lookup.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace Kiboko\Component\Flow\Magento2;
 
 use Kiboko\Component\Bucket\AcceptanceResultBucket;
-use Kiboko\Component\Bucket\ComplexResultBucket;
 use Kiboko\Component\Bucket\RejectionResultBucket;
 use Kiboko\Contract\Mapping\CompiledMapperInterface;
 use Kiboko\Contract\Pipeline\TransformerInterface;
 use Psr\SimpleCache\CacheInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
-final class CategoryLookup implements TransformerInterface
+final class Lookup implements TransformerInterface
 {
     public function __construct(
         private \Psr\Log\LoggerInterface $logger,
         private \Kiboko\Magento\V2_1\Client|\Kiboko\Magento\V2_2\Client|\Kiboko\Magento\V2_3\Client|\Kiboko\Magento\V2_4\Client $client,
         private CacheInterface $cache,
+        private string $cacheKey,
         private CompiledMapperInterface $mapper,
         private SerializerInterface $serializer,
         private string $mappingField,
@@ -29,11 +29,7 @@ final class CategoryLookup implements TransformerInterface
         $line = yield;
         while (true) {
             try {
-                $lookup = $this->serializer->deserialize(
-                    $this->cache->get(sprintf('category.%s', $line[$this->mappingField])),
-                    \Kiboko\Magento\V2_3\Model\EavDataAttributeOptionInterface::class,
-                    'json',
-                );
+                $lookup = $this->cache->get(sprintf($this->cacheKey, $line[$this->mappingField]));
 
                 if ($lookup === null) {
                     $lookup = $this->client->catalogCategoryAttributeOptionManagementV1GetItemsGet(
@@ -49,8 +45,8 @@ final class CategoryLookup implements TransformerInterface
                     }
 
                     $this->cache->set(
-                        sprintf('category.%s', $line[$this->mappingField]),
-                        $this->serializer->serialize($lookup, 'json'),
+                        sprintf($this->cacheKey, $line[$this->mappingField]),
+                        $lookup = $this->serializer->serialize($lookup, 'json'),
                     );
                 }
             } catch (\RuntimeException $exception) {
