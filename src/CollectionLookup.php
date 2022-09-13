82f048651896ea07f9b5099dccaabd35ddda6784
sebprt <sebastien@kiboko.fr>
2022-09-13 16:32:59 +0200

    Updated lookups

--- src/CollectionLookup.php
+++ src/CollectionLookup.php
@@ -4,48 +4,64 @@ declare(strict_types=1);
 
 namespace Kiboko\Component\Flow\Magento2;
 
+use Kiboko\Component\Bucket\AcceptanceResultBucket;
 use Kiboko\Component\Bucket\ComplexResultBucket;
+use Kiboko\Component\Bucket\RejectionResultBucket;
+use Kiboko\Contract\Mapping\CompiledMapperInterface;
 use Kiboko\Contract\Pipeline\TransformerInterface;
+use Psr\SimpleCache\CacheInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 final class CollectionLookup implements TransformerInterface
 {
     public function __construct(
         private \Psr\Log\LoggerInterface $logger,
         private \Kiboko\Magento\V2_1\Client|\Kiboko\Magento\V2_2\Client|\Kiboko\Magento\V2_3\Client|\Kiboko\Magento\V2_4\Client $client,
+        private CacheInterface $cache,
+        private CompiledMapperInterface $mapper,
+        private SerializerInterface $serializer,
+        private string $mappingField,
     ) {
     }
 
     public function transform(): \Generator
     {
         $line = yield;
-        do {
-            $bucket = new ComplexResultBucket();
-            $output = $line;
-
+        while (true) {
             try {
-                $lookup = $this->client->catalogCategoryAttributeOptionManagementV1GetItemsGet(
-                    attributeCode: $line["Collection"],
-                );
+                $lookup = $this->serializer->deserialize(
+                    $this->cache->get(sprintf('collection.%s', $line[$this->mappingField])),
+                    \Kiboko\Magento\V2_3\Model\EavDataAttributeOptionInterface::class,
+                    'json'
+            );
 
-                if (!$lookup instanceof \Kiboko\Magento\V2_1\Model\EavDataAttributeOptionInterface
-                    && !$lookup instanceof \Kiboko\Magento\V2_2\Model\EavDataAttributeOptionInterface
-                    && !$lookup instanceof \Kiboko\Magento\V2_3\Model\EavDataAttributeOptionInterface
-                    && !$lookup instanceof \Kiboko\Magento\V2_4\Model\EavDataAttributeOptionInterface
-                ) {
-                    return;
+                if ($lookup === null) {
+                    $lookup = $this->client->catalogCategoryAttributeOptionManagementV1GetItemsGet(
+                        attributeCode: $line[$this->mappingField],
+                    );
+
+                    if (!$lookup instanceof \Kiboko\Magento\V2_1\Model\EavDataAttributeOptionInterface
+                        && !$lookup instanceof \Kiboko\Magento\V2_2\Model\EavDataAttributeOptionInterface
+                        && !$lookup instanceof \Kiboko\Magento\V2_3\Model\EavDataAttributeOptionInterface
+                        && !$lookup instanceof \Kiboko\Magento\V2_4\Model\EavDataAttributeOptionInterface
+                    ) {
+                        return;
+                    }
+
+                    $this->cache->set(
+                        sprintf('collection.%s', $line[$this->mappingField]),
+                        $this->serializer->serialize($lookup, 'json'),
+                    );
                 }
             } catch (\RuntimeException $exception) {
                 $this->logger->warning($exception->getMessage(), ['exception' => $exception, 'item' => $line]);
-                $bucket->reject($line);
-                return;
+                $line = yield new RejectionResultBucket($line);
+                continue;
             }
 
-            $output = (function () use ($lookup, $output) {
-                $output['Collection'] = $lookup->getLabel();
-                return $output;
-            })();
+            $output = ($this->mapper)($lookup, $line);
 
-            $bucket->accept($output);
-        } while ($line = (yield $bucket));
+            $line = yield new AcceptanceResultBucket($output);
+        }
     }
 }
