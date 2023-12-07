59dc7e2c56f884f9f674334903010cdd0264225d
Jonathan <jonathan@kiboko.fr>
2023-12-07 15:17:38 +0100

    Add logical cursor for long query parameters

--- src/FilterGroup.php
+++ src/FilterGroup.php
@@ -7,6 +7,9 @@ namespace Kiboko\Component\Flow\Magento2;
 class FilterGroup
 {
     private array $filters = [];
+    private array $longFilters = [];
+    private int $offset = 0;
+    private int $lenght = 200;
 
     public function withFilter(Filter $filter): self
     {
@@ -15,6 +18,20 @@ class FilterGroup
             'value' => $filter->value,
             'condition_type' => $filter->conditionType,
         ];
+
+        return $this;
+    }
+
+    public function withLongFilter(Filter $filter, int $offset = 0, int $lenght = 200): self
+    {
+        $this->longFilters[] = [
+            'field' => $filter->field,
+            'value' => $filter->value,
+            'condition_type' => $filter->conditionType,
+        ];
+
+        $this->offset = $offset;
+        $this->lenght = $lenght;
 
         return $this;
     }
@@ -28,6 +45,25 @@ class FilterGroup
         ]);
 
         return $this;
+    }
+
+    private function sliceLongFilter(string $value): iterable
+    {
+        $iterator = new \ArrayIterator(explode(',', $value));
+        while($this->offset < iterator_count($iterator)) {
+            $filteredValue = array_slice(iterator_to_array($iterator), $this->offset, $this->lenght);
+            $this->offset += $this->lenght;
+            yield $filteredValue;
+        }
+    }
+
+    public function compileLongFilters(int $groupIndex = 0)
+    {
+        return array_merge(...array_map(fn (array $item, int $key) => [
+            sprintf('searchCriteria[filterGroups][%s][filters][%s][field]', $groupIndex, $key) => $item['field'],
+            sprintf('searchCriteria[filterGroups][%s][filters][%s][value]', $groupIndex, $key) => iterator_to_array($this->sliceLongFilter($item['value'])),
+            sprintf('searchCriteria[filterGroups][%s][filters][%s][conditionType]', $groupIndex, $key) => $item['condition_type'],
+        ], $this->longFilters, array_keys($this->longFilters)));
     }
 
     public function compileFilters(int $groupIndex = 0): array
